23d7a0045f3da1972dae38e171ec77fbe412280b
Shaked Amar <shakeda@axonize.com>
2019-03-04 21:18:16 +0200

    Updates

---
+++ precache-manifest.1ac054339b489d6c995750147e38b50f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b26d9051054f56aea700",
+    "url": "/memoz/static/css/main.ad92a712.chunk.css"
+  },
+  {
+    "revision": "b26d9051054f56aea700",
+    "url": "/memoz/static/js/main.b26d9051.chunk.js"
+  },
+  {
+    "revision": "7fccf978a02fa92fef42",
+    "url": "/memoz/static/js/runtime~main.7fccf978.js"
+  },
+  {
+    "revision": "5dc65c04c5ae3f3bbc8f",
+    "url": "/memoz/static/js/2.5dc65c04.chunk.js"
+  },
+  {
+    "revision": "9128c162ddaeba7a33db110fff7343b4",
+    "url": "/memoz/index.html"
+  }
+];
